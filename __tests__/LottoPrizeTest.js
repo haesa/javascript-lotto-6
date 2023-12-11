71606eb2740c2ec11f4e77f7c18096a76b3c45f8
haesa <xdhaesa@gmail.com>
2023-12-11 11:00:54 +0900

    [Test]: prize가 third일 때 케이스 추가

--- __tests__/LottoPrizeTest.js
+++ __tests__/LottoPrizeTest.js
@@ -4,8 +4,9 @@ describe('LottoPrize 테스트', () => {
   test('당첨된 로또의 등수를 구하는 테스트', () => {
     const lottoPrize = LottoPrize.determinePrizeTier([
       { matchCount: 5, hasBonus: true },
+      { matchCount: 5, hasBonus: false },
       { matchCount: 3, hasBonus: false },
     ]);
-    expect(lottoPrize).toEqual(['second', 'fifth']);
+    expect(lottoPrize).toEqual(['second', 'third', 'fifth']);
   });
 });
