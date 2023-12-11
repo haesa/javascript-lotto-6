06417533e2411f2f22679eadf3d5f6248f69cb5d
haesa <xdhaesa@gmail.com>
2023-12-11 10:13:57 +0900

    [Test]: calculateProfit 메서드가 private으로 변경돼서 테스트 삭제

--- __tests__/LottoProfitTest.js
+++ __tests__/LottoProfitTest.js
@@ -1,11 +1,6 @@
 import LottoProfit from '../src/Domain/LottoProfit';
 
 describe('LottoProfit 테스트', () => {
-  test('로또 수익 테스트', () => {
-    const profit = LottoProfit.calculateProfit(['fifth', 'fourth']);
-    expect(profit).toBe(55000);
-  });
-
   test('로또 수익률 테스트', () => {
     const lottoYield = LottoProfit.calculateYield(
       ['fifth', 'fifth', 'fifth'],
